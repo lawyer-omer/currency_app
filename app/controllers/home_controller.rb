5d61b7d7f19f7619edf46cad798e6a7fee4ec563
lawyer-omer <av.omeraydin29@gmail.com>
2022-06-29 16:42:43 +0300

    API Errors fixed (empty or wrong param situations)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -31,39 +31,37 @@ class HomeController < ApplicationController
     end
 
     def manage
-        @fromparam = params[:fromcurrency]
-        @toparam = params[:tocurrency]
+        @fromparam = params[:fromcurrency].to_s
+        @toparam = params[:tocurrency].to_s
+        @fromparam = @fromparam.upcase
+        @toparam = @toparam.upcase
 
         if params[:fromcurrency] && params[:tocurrency] == ""
-            @fromparam && @toparam = "N/A"
+            @from_output = "N/A"
+            @to_output = "N/A"
+            @rate_output = "N/A"
         elsif params[:fromcurrency] && params[:tocurrency]
             require 'net/http'
             require 'json'
-        
-            currency_to = "usd"
-            currency_from = "eur"
             
-            @url = "https://api.apilayer.com/fixer/convert?to=#{@fromparam}&from=#{@toparam}&amount=100&apikey=JFkntXPV2tLSXgKBFUdYra5ZHtLlzKF3"
+            @url = "https://api.apilayer.com/fixer/convert?to=#{@toparam}&from=#{@fromparam}&amount=100&apikey=JFkntXPV2tLSXgKBFUdYra5ZHtLlzKF3"
             @uri = URI(@url)
             
             @response = Net::HTTP.get(@uri)
             @currency = JSON.parse(@response)
-        
-            # Check for empty inputs
-        
-            if @currency.empty?
-                @from_output = "N/A (You need to enter a pair to calculate rate"
-                @to_output = "N/A (You need to enter a pair to calculate rate"
-                @rate_output = "N/A (You need to enter a pair to calculate rate"
-            elsif !@currency
-                @from_output = "N/A (You need to enter a pair to calculate rate"
-                @to_output = "N/A (You need to enter a pair to calculate rate"
-                @rate_output = "N/A (You need to enter a pair to calculate rate"
-            else
+    
+            @from_output = @currency["query"]["from"]
+            @to_output = @currency["query"]["to"]
+
+            if  @fromparam == @from_output && @toparam == @to_output
+
                 @from_output = @currency["query"]["from"]
                 @to_output = @currency["query"]["to"]
                 @rate_output = @currency["info"]["rate"]
+                
             end
+
+
         end
 
     end
