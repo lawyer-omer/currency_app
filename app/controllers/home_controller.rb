aa3c21e61c33f63a7d977234883a19c1fd8a92c1
lawyer-omer <av.omeraydin29@gmail.com>
2022-06-29 11:40:06 +0300

    Initialize API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
     def index
+
+        require 'net/http'
+        require 'json'
+    
+        @url = 'https://api.apilayer.com/fixer/convert?to=usd&from=eur&amount=100&apikey=B9hzo2vxHrbvTcrNpgJg3z8jGaMxS0UC'
+        @uri = URI(@url)
+    
+        @response = Net::HTTP.get(@uri)
+        @currency = JSON.parse(@response)
+
+        
     end
 end
